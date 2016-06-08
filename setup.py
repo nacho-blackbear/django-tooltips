d90f4715bed44b0e1f9dd3e28a2186148bc3289e
Nacho Nachev <nacho@blackbear.bg>
2016-06-08 14:56:59 +0300

    Migrate project to support Django 1.7+

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, ImportError):
 
 setup(
     name = 'django-tooltips',
-    version = '1.1.5',
+    version = '1.1.6',
     description='Django manageable Bootstrap Tooltips',
     long_description=description,
     author = 'Sander van de Graaf',
--- tooltips/migrations/0001_initial.py
+++ tooltips/migrations/0001_initial.py
@@ -1,40 +1,23 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
 
+from django.db import models, migrations
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Tooltip'
-        db.create_table(u'tooltips_tooltip', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('url', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
-            ('selector', self.gf('django.db.models.fields.CharField')(default='', max_length=50)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('body', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal(u'tooltips', ['Tooltip'])
+    dependences = []
 
-
-    def backwards(self, orm):
-        # Deleting model 'Tooltip'
-        db.delete_table(u'tooltips_tooltip')
-
-
-    models = {
-        u'tooltips.tooltip': {
-            'Meta': {'object_name': 'Tooltip'},
-            'body': ('django.db.models.fields.TextField', [], {}),
-            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'selector': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
-        }
-    }
-
-    complete_apps = ['tooltips']
+    operations = [
+        migrations.CreateModel(
+            name='Tooltip',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('url', models.CharField(max_length=255, db_index=True)),
+                ('selector', models.CharField(max_length=50, default='', blank=False, null=False, help_text='div:contains("example")')),
+                ('title', models.CharField(max_length=255)),
+                ('body', models.TextField()),
+            ],
+            bases=(models.Model,),
+        )
+    ]
